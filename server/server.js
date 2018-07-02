bfff89994d89eff485828cb24722f738b7a06893
kiteena <kristinamilkovich@gmail.com>
2018-07-01 21:29:41 -0700

    Fixed syntax error in PORT env variable

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const publicPath = path.join(__dirname, '../public');
 // console.log(__dirname +'/../public');
 // console.log(publicPath);
 
-const port = process.env.port || 3000; 
+const port = process.env.PORT || 3000; 
 var app = express(); 
 
 app.use(express.static(publicPath))
