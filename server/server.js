a82d277c9b667462f7fdba9fb80e81ed3c8a785c
kiteena <kristinamilkovich@gmail.com>
2018-07-01 21:16:00 -0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require('path'); 
+const express = require('express'); 
+
+const publicPath = path.join(__dirname, '../public');
+console.log(__dirname +'/../public');
+console.log(publicPath);
+
+const port = process.env.port || 3000; 
+var app = express(); 
+
+app.use(express.static(publicPath))
+
+app.get('/', (req, res)=>{
+    res.send('index.html');
+});
+
+app.listen(port, ()=>{ 
+    console.log('listening on port ${port}');
+});
